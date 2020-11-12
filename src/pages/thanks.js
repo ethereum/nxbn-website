d86f77f46cfe72c56e6ef06a1ffcb9ad22ee46c5
Sam Richards <sbrichards@gmail.com>
2020-11-12 11:45:12 -0800

    Hide inquire page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,19 +1,19 @@
-import React from "react"
+// import React from "react"
 
-import PageMetadata from "../components/PageMetadata"
-import { PageBodyNoBanner } from "../components/SharedStyledComponents"
+// import PageMetadata from "../components/PageMetadata"
+// import { PageBodyNoBanner } from "../components/SharedStyledComponents"
 
-const ThankYouPage = () => {
-  return (
-    <>
-      <PageMetadata title="Thanks" />
-      <PageBodyNoBanner>
-        <h1>Thank you!</h1>
-        <p>{/* <FormattedMessage id="page-thanks.p-1" /> */}</p>
-        <p>{/* <FormattedMessage id="page-thanks.p-2" /> */}</p>
-      </PageBodyNoBanner>
-    </>
-  )
-}
+// const ThankYouPage = () => {
+//   return (
+//     <>
+//       <PageMetadata title="Thanks" />
+//       <PageBodyNoBanner>
+//         <h1>Thank you!</h1>
+//         <p>{/* <FormattedMessage id="page-thanks.p-1" /> */}</p>
+//         <p>{/* <FormattedMessage id="page-thanks.p-2" /> */}</p>
+//       </PageBodyNoBanner>
+//     </>
+//   )
+// }
 
-export default ThankYouPage
+// export default ThankYouPage
