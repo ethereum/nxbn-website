3e7f4c731cf69e7e5c9ac59a13ea505218373688
Pablo Pettinari <pettinarip@gmail.com>
2022-04-15 16:15:53 -0300

    remove analytics plugin since it is not used anymore

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,13 +94,6 @@ module.exports = {
         fonts: [`Work Sans\:100,300,400,500,600,700`],
       },
     },
-    // TODO need different plugin to support new Measurement IDs?
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "G-1BJFN30WTF",
-      },
-    },
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
