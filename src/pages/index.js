a91281a9129214444758d674cca2fd7751a4c3e9
gichiba <griffin.hotchkiss@gmail.com>
2022-11-17 10:14:10 +0700

    small typo in Geoffrey's profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -341,7 +341,7 @@ class IndexPage extends React.Component {
                     <b>Geoffrey See </b>
                     is co-founder of startup 
                     {" "}
-                    <Link to="https://www.poko.fund/">Poko</Link>
+                    <Link to="https://www.poko.fund/">Poko. </Link>
                      He has extensive experience working on the legal and regulatory side of the blockchain, 
                      as well as in an entrepreneurial education initiative and the burgeoning field of DAOs. 
                      For his fellowship project, Geoffrey will be exploring the interface 
