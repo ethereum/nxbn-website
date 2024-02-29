7f7d43719798f7851d30c22123f39f299cd731d7
Corwin Smith <cssmittys@gmail.com>
2024-02-29 15:01:04 -0700

    setup chakra

---
+++ src/@chakra-ui/foundations/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  background: '#056589',
+  backgroundHighlight: '#064A64',
+  highlight: '#2C7DC5',
+  body: '#F0F7FF',
+  bodyHover: '#B0D4FF',
+  action: '#B3E678',
+  actionHover: '#CFF8A0',
+  actionHighlight: '#6F9D39'
+}
