1a3fa906a85843fbbf1c609b566e340fca9bc2e2
Corwin Smith <cssmittys@gmail.com>
2024-03-26 22:07:53 -0600

    setup component

---
+++ src/components/PreviousEditions.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import ContentContainer from '@/components/ContentContainer'
+import { H2 } from '@/components/Headings'
+
+const PreviousEditions = () => {
+  return (
+    <>
+      <ContentContainer>
+        <H2 variant="action">PreviousEditions</H2>
+      </ContentContainer>
+    </>
+  )
+}
+
+export default PreviousEditions
