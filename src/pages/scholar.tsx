ae997313cfce368f6abdaea332882e6a1b1f7cc7
Corwin Smith <cssmittys@gmail.com>
2024-04-15 14:48:14 -0600

    Update src/pages/scholar.tsx
    Co-authored-by: Pablo Pettinari <pettinarip@gmail.com>

--- src/pages/scholar.tsx
+++ src/pages/scholar.tsx
@@ -4,7 +4,7 @@ import ScholarOpen from "@/layouts/ScholarOpen"
 import { SCHOLAR_PROGRAM_OPEN } from "@/utils/constants"
 
 const ScholarPage = () => {
-  return <>{SCHOLAR_PROGRAM_OPEN ? <ScholarOpen /> : <ScholarClosed />}</>
+  return SCHOLAR_PROGRAM_OPEN ? <ScholarOpen /> : <ScholarClosed />
 }
 
 export default ScholarPage
