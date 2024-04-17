531c3f3f2107b9cf6f363c2a7a98da24b24ba03c
Nuno Loureiro <loureironuno@gmail.com>
2024-04-17 11:31:05 +0100

    fix the broken not centred laytout

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Container } from "@chakra-ui/react"
 
 const ContentContainer = (props) => {
-  return <Container maxW="1536px" {...props} />
+  return <Container maxW="1536px" {...props} margin="0 auto"/>
 }
 
 export default ContentContainer
