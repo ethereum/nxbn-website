7fcfe717ac209a1cc9d8b6f3683f0cd4f308570c
Corwin Smith <cssmittys@gmail.com>
2024-05-29 12:14:03 -0600

    test out outputFileTracingExcludes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
 const withMDX = require("@next/mdx")()
 
 /** @type {import('next').NextConfig} */
@@ -9,4 +10,32 @@ const nextConfig = {
   // Optionally, add any other Next.js config below
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = withMDX((phase) => {
+  if (phase !== PHASE_DEVELOPMENT_SERVER) {
+    nextConfig = {
+      ...nextConfig,
+      experimental: {
+        ...experimental,
+        outputFileTracingExcludes: {
+          "*": [
+            /**
+             * Exclude these paths from the trace output to avoid bloating the
+             * Netlify functions bundle.
+             *
+             * @see https://github.com/orgs/vercel/discussions/103#discussioncomment-5427097
+             * @see https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
+             */
+            "node_modules/@swc/core-linux-x64-gnu",
+            "node_modules/@swc/core-linux-x64-musl",
+            "node_modules/@esbuild/linux-x64",
+            "public/**/*.png",
+            "public/**/*.jpg",
+            "public/**/*.gif",
+          ],
+        },
+      },
+    }
+  }
+
+  return nextConfig
+})
