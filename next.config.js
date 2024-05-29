cd1f046ed57cf27df33d77afcc17f22fd42f18b7
Corwin Smith <cssmittys@gmail.com>
2024-05-29 12:48:43 -0600

    test CPU config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,32 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
-const withMDX = require("@next/mdx")()
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const withMDX = require("@next/mdx")();
+
+const LIMIT_CPUS = Number(process.env.LIMIT_CPUS ?? 2)
+
+const experimental = LIMIT_CPUS
+  ? {
+      // This option could be enabled in the future when flagged as stable, to speed up builds
+      // (see https://nextjs.org/docs/pages/building-your-application/configuring/mdx#using-the-rust-based-mdx-compiler-experimental)
+      // mdxRs: true,
+
+      // Reduce the number of cpus and disable parallel threads in prod envs to consume less memory
+      workerThreads: false,
+      cpus: LIMIT_CPUS,
+    }
+  : {}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   // Configure `pageExtensions` to include MDX files
-  pageExtensions: ["md", "ts", "tsx"],
+  pageExtensions: ["md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
 }
 
 module.exports = withMDX((phase) => {
   if (phase !== PHASE_DEVELOPMENT_SERVER) {
-    nextConfig = {
+    return {
       ...nextConfig,
       experimental: {
         ...experimental,
@@ -25,7 +39,6 @@ module.exports = withMDX((phase) => {
              * @see https://github.com/orgs/vercel/discussions/103#discussioncomment-5427097
              * @see https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
              */
-            ".git/**",
             "node_modules/@swc/core-linux-x64-gnu",
             "node_modules/@swc/core-linux-x64-musl",
             "node_modules/@esbuild/linux-x64",
@@ -38,5 +51,5 @@ module.exports = withMDX((phase) => {
     }
   }
 
-  return nextConfig
-})
+  return nextConfig;
+});
